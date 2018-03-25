0903246f95f5f5056d0105b1e4240ed4afb11bd6
Mathieu-S <mathieusartorio@gmail.com>
2018-03-25 13:46:33 +0200

    Ajout route vers backoffice admin

---
+++ src/Controller/BackOffice/Admin/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\BackOffice\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home", name="adminHome")
+     */
+    public function IndexAction()
+    {
+        return $this->render('backoffice/admin/index.html.twig', []);
+    }
+}
