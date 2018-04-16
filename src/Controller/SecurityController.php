68a243cbb49ef2a03f8e5d33c1dd6fae107e661d
Mathieu-S <mathieusartorio@gmail.com>
2018-04-16 22:20:41 +0200

    Ajout du choix de création de compte

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,10 +26,50 @@ class SecurityController extends Controller
     /**
      * @Route("/register", name="security_register")
      */
-    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function registerAction()
+    {
+        return $this->render('security/register-selector.html.twig');
+    }
+
+    /**
+     * @Route("/register-user", name="security_register_user")
+     */
+    public function registerUserAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         $user = new User();
         $roles[] = 'ROLE_USER';
+        $user->setRoles($roles);
+
+        $form = $this->createForm('App\Form\UserType', $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $this->get('security.token_storage')->setToken($token);
+            $request->getSession()->set('_security_main', serialize($token));
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'userForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/register-hotel", name="security_register_hotel")
+     */
+    public function registerHotelAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $roles[] = 'ROLE_HOTEL';
         $user->setRoles($roles);
 
         $form = $this->createForm('App\Form\UserType', $user);
