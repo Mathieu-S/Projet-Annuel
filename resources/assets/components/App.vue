6e8e61ef7d69d4afb7ff4480515edeb4fa4e68d6
Mathieu SARTORIO <Mathieu.SARTORIO@ebp.com>
2018-04-27 14:52:23 +0200

    creéation de la structure pour l'affichage des composants

--- resources/assets/components/App.vue
+++ resources/assets/components/App.vue
@@ -1,10 +1,18 @@
 <template>
-    <section>
-        <router-link to="/foo">Aller à Foo</router-link>
-        <router-link to="/bar">Aller à Bar</router-link>
-        <br>
-        <router-view/>
-    </section>
+    <article id="hotel-list" class="row">
+        <div class="col-3">
+            <div class="card">
+                Hotel filtre
+                <router-link to="/foo">Aller à Foo</router-link>
+                <router-link to="/bar">Aller à Bar</router-link>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="card">
+                <router-view/>
+            </div>
+        </div>
+    </article>
 </template>
 
 <script lang="ts">
