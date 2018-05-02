771b5c342a95c0916efdaec95efa45de691209cd
Mathieu-S <mathieusartorio@gmail.com>
2018-05-02 22:36:03 +0200

    Ajout fonction CRUD pour les chambre (WIP)

--- src/Controller/BackOffice/Hotelier/ChambreController.php
+++ src/Controller/BackOffice/Hotelier/ChambreController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\BackOffice\Hotelier;
 
+use App\Entity\BedRoom;
 use App\Entity\Hotel;
+use App\Form\BedRoomType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -44,5 +47,58 @@ class ChambreController extends Controller
             'chambres' => $chambres,
             'hotel' => $hotel
         ]);
+    }
+
+    /**
+     * @Route("/create", name="hotelierCreateHotelChambers")
+     */
+    public function CreateHotelChambersAction(Request $request)
+    {
+        $bedRoom = new BedRoom();
+        $form = $this->createForm(BedRoomType::class, $bedRoom);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bedRoom->setAvailability(true);
+//            $bedRoom->set($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($bedRoom);
+            $em->flush();
+            return $this->redirectToRoute('hotelierHome');
+
+        }
+        return $this->render('backoffice/hotelier/chambres/form.html.twig', [
+            'bedRoomForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="hotelierEditHotels")
+     */
+    public function EditHotelChambersAction(Request $request, Hotel $hotel)
+    {
+        $form = $this->createForm(HotelType::class, $hotel);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('hotelierHome');
+        }
+        return $this->render('backoffice/hotelier/hotels/form.html.twig', [
+            'hotelForm' => $form->createView()
+        ]);
+    }
+    /**
+     * @Route("/delete/{id}", name="hotelierDeleteHotels")
+     */
+    public function DeleteHotelChambersAction(Hotel $hotel)
+    {
+        if ($hotel === null) {
+            return $this->redirectToRoute('hotelierHome');
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($hotel);
+        $em->flush();
+        return $this->redirectToRoute('hotelierHome');
     }
 }
