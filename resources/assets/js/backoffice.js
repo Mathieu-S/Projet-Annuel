2c0ab49ea6b9a0c4b890e0a2e5440fb7afd59eb7
sokstephane <sokstephane12@gmail.com>
2018-05-04 22:27:27 +0200

    loading in js cities dynamically according to the selected department

--- resources/assets/js/backoffice.js
+++ resources/assets/js/backoffice.js
@@ -14,25 +14,36 @@ $(document).ready(function () {
 
     region.change(function() {
 
-        // empty subzone and reset the default selected value to Département
+        // empty department and reset the default selected value to Département
         department.empty().append($('<option>', {value: ""}).text("Département"));
-        // empty city and reset the default selected value to Ville
 
+        // empty city and reset the default selected value to Ville
         city.empty().append($('<option>', {value: ""}).text("Ville"));
+
         handleSelectDisplay();
 
-        // filling subzone select according to the selected zone
+        // filling department select according to the selected region
         $.get($(this).data("autocomplete-url") + "/" + $(this).val(), function (data) {
             $.each(data, function (key, value) {
                 department.append($('<option>', {value: value}).text(key));
             });
         });
 
-       // handleSelectDisplay();
     });
 
     department.change(function() {
-       handleSelectDisplay();
+
+        // empty city and reset the default selected value to Ville
+        city.empty().append($('<option>', {value: ""}).text("Ville"));
+
+        handleSelectDisplay();
+
+        // filling city select according to the selected department
+        $.get($(this).data("autocomplete-url") + "/" + $(this).val(), function (data) {
+            $.each(data, function (key, value) {
+                city.append($('<option>', {value: value}).text(key));
+            });
+        });
     });
 
     function handleSelectDisplay() {
