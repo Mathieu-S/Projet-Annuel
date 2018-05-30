070eff28b92b7755c2b00b392cc0485ae1952ef9
sokstephane <sokstephane12@gmail.com>
2018-05-30 22:54:50 +0200

    creation de la méthode de création d'une reservation, la chambre en question devient indisponible

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BedRoom;
+use App\Entity\Reservation;
+use App\Form\ReservationType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationController extends Controller
+{
+    /**
+     * @Route("/reservation/{id}", name="create_reservation")
+     */
+    public function CreateAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $bedRoomId = $request->attributes->get('id');
+        $bedRoom = $em
+            ->getRepository('App:BedRoom')
+            ->findOneBy(['id' => $bedRoomId]);
+        $reservation = new Reservation();
+
+        $reservationForm = $this->createForm(ReservationType::class, $reservation);
+        $reservationForm->handleRequest($request);
+
+        if ($reservationForm->isSubmitted() && $reservationForm->isValid()) {
+
+            $reservation->setBedRoom($bedRoom)
+                        ->setUser($this->getUser());
+            $bedRoom->setAvailability(false);
+
+            $em->persist($reservation);
+            $em->flush();
+            return $this->render('home/index.html.twig', [
+                'bedRoom' => $bedRoom,
+                'reservationForm' => $reservationForm->createView()
+            ]);
+        }
+
+        return $this->render('reservation/form.html.twig', [
+            'bedRoom' => $bedRoom,
+            'reservationForm' => $reservationForm->createView()
+        ]);
+    }
+
+
+}
