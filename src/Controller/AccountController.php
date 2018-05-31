acd0eb464f28e21105da7b3c8fa9b3add429d683
sokstephane <sokstephane12@gmail.com>
2018-05-31 11:44:46 +0200

    ajout des méthodes de récupération des réservations et de l'utilisateur pour la partie de l'espace personnel dans le controller AccountController

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,22 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/account")
+ * @Security("is_granted(['ROLE_HOTEL', 'ROLE_ADMIN', 'ROLE_USER'])")
+ */
 class AccountController extends Controller
 {
     /**
-     * @Route("/account/{id}", name="indexAccount")
-     * @Security("is_granted(['ROLE_HOTEL', 'ROLE_ADMIN', 'ROLE_USER'])")
+     * @Route("/user/{id}", name="personalDataAccount")
      */
-    public function IndexAction(Request $request)
+    public function UserDataAction(Request $request)
     {
 
+        $em = $this->getDoctrine()->getManager();
 
+        $userId = $request->attributes->get('id');
+        $user = $em
+            ->getRepository('App:User')
+            ->findOneBy(['id' => $userId]);
+
+        return $this->render('account/personal-data.html.twig', [
+            'user' => $user
+        ]);
     }
 
+    /**
+     * @Route("/reservation/{id}", name="reservationDataAccount")
+     */
+    public function ReservationDataAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $userId = $request->attributes->get('id');
+        $reservations = $em
+            ->getRepository('App:Reservation')
+            ->findBy(['user' => $userId]);
+        return $this->render('account/reservation.html.twig', [
+            'reservations' => $reservations
+        ]);
+
+    }
 
 }
