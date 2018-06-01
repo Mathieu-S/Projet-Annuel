7262eca59dd18ebb9ddd01bdd5b13231b6c5fbb2
sokstephane <sokstephane12@gmail.com>
2018-06-01 10:47:53 +0200

    Restriction d'accès à la page: accessible seulement si l'on est authentifiés. Méthode de création d'une demande de contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/contact")
+ * @Security("is_granted(['ROLE_HOTEL', 'ROLE_ADMIN', 'ROLE_USER'])")
+ */
 class ContactController extends Controller
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/create", name="createContact")
      */
-    public function IndexAction()
+    public function IndexAction(Request $request)
     {
-        return $this->render('contact/index.html.twig', []);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $contact = new Contact();
+
+        $contactForm = $this->createForm(ContactType::class, $contact);
+        $contactForm->handleRequest($request);
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+
+            $dateNow = new \DateTime('now');
+            $contact->setCreatedAt($dateNow);
+            $contact->setUser($this->getUser());
+
+            $em->persist($contact);
+            $em->flush();
+            return $this->render('home/index.html.twig', [
+            ]);
+        }
+
+        return $this->render('contact/form.html.twig', [
+            'contactForm' => $contactForm->createView()
+        ]);
+
     }
 }
