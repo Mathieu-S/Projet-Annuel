88c80b22308db065d7676e7d995089adf85688b0
sokstephane <sokstephane12@gmail.com>
2018-06-01 09:57:03 +0200

    ContactType Form

---
+++ src/Form/ContactType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet'
+            ])
+            ->add('message', TextType::class, [
+                'label' => 'Message'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // uncomment if you want to bind to a class
+            'data_class' => Contact::class
+        ]);
+    }
+}
