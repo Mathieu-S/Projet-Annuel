5785abe6bba6f2a8b2236dfb5db3b5daab369cfa
Mathieu-S <mathieusartorio@gmail.com>
2018-06-03 15:35:41 +0200

    Ajout du filtre prix (html)

--- resources/assets/components/App.vue
+++ resources/assets/components/App.vue
@@ -8,11 +8,23 @@
                         <input class="form-control form-control-sm" type="text" v-model="searchInput" placeholder="Nom hôtel, Ville">
                     </div>
 
-                    <div class="form-check" v-for="optionBedRoom in optionsBedRoom" :key="optionBedRoom.id">
-                        <input class="form-check-input" type="checkbox" v-model="selectedOptions" :value="optionBedRoom.name">
-                        <label class="form-check-label">
-                            {{optionBedRoom.name}}
+                    <div class="form-group">
+                        <label for="formControlRange">
+                            Prix maximale :
+                            <span v-if="maxPrice === 0"> pas de limite</span>
+                            <span v-else>{{ maxPrice }} €</span>
                         </label>
+                        <input type="range" class="form-control-range" id="formControlRange" min="20" max="300" step="10" v-model="maxPrice">
+                    </div>
+
+                    <div class="form-group">
+                        Options :
+                        <div class="form-check" v-for="optionBedRoom in optionsBedRoom" :key="optionBedRoom.id">
+                            <input class="form-check-input" type="checkbox" v-model="selectedOptions" :value="optionBedRoom.name">
+                            <label class="form-check-label">
+                                {{optionBedRoom.name}}
+                            </label>
+                        </div>
                     </div>
                 </form>
             </div>
