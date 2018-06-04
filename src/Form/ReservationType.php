1eacfc7efc8f443520aa19f4419e326bc3ab9864
sokstephane <sokstephane12@gmail.com>
2018-06-05 00:07:47 +0200

    messages d'erreur reservationType

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -7,6 +7,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReservationType extends AbstractType
@@ -20,6 +23,18 @@ class ReservationType extends AbstractType
             ->add('finalDate', DateType::class, [
                 'label' => 'Dernier jour de réservation'
             ])
+            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+                $form = $event->getForm();
+                $currentDate = new \DateTime('now');
+                $data = $event->getData();
+                if ($data->getStartDate() >= $data->getFinalDate()) {
+                    $form->get('finalDate')->addError(new FormError("La date d'arrivée doit forcément être plus tard que la date de départ"));
+                }
+                if ($data->getStartDate() <= $currentDate) {
+                    $form->get('startDate')->addError(new FormError("Vous ne pouvez pas réserver pour une date antérieure à celle d'aujourd'hui"));
+                }
+
+            });
         ;
     }
 
